03a131e7aa8b9b9124acdcba9eb2922a42d974e6
tinyheros <tinyherosgame@gmail.com>
2024-05-16 02:29:24 -0700

    first commit

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,30 @@
+async function detectWallet() {
+	const provider = window.phantom?.solana;
+	return provider?.isPhantom
+}
+
+async function connectWallet(){
+	const provider = window.phantom?.solana;
+	if(!provider){
+		alert("No wallet detected.")	
+	} else {	
+ 		const res = await window.solana.connect()
+ 		return res.publicKey
+ }
+}
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event79_Act2(runtime, localVars)
+	{
+		const wallet = await detectWallet()
+		if(!wallet) return alert("No wallet detected")
+		const address = await connectWallet()
+		runtime.globalVars.defaultAddress = address
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
